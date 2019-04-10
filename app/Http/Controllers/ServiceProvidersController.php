747fd3ea5ce28cb52852926a124727d185faabe0
Reuben Wafula <rubewafula@gmail.com>
2019-04-11 01:54:28 +0300

    add me

--- app/Http/Controllers/ServiceProvidersController.php
+++ app/Http/Controllers/ServiceProvidersController.php
@@ -64,12 +64,12 @@ class ServiceProvidersController extends Controller{
             . " sp.work_lat, sp.work_lng, sp.status_id, sp.overall_rating, "
             . " sp.overall_likes, sp.overall_dislikes, sp.created_at, sp.updated_at, "
             . " d.id_number, d.date_of_birth, d.gender, "
-            . " concat( '$image_url' , if(d.passport_photo is null, 'avatar-bg-1.png', "
+            . " concat( '$image_url' ,'/', if(d.passport_photo is null, 'avatar-bg-1.png', "
             . " json_extract(d.passport_photo, '$.media_url')) ) as thumbnail, "
             . " concat( '$sp_providers_url' , '/', if(sp.cover_photo is null, 'img-03.jpg', "
             . " json_extract(sp.cover_photo, '$.media_url'))) as cover_photo, "
             . " d.home_location, work_phone_no, sp.business_description "
-            . " FROM service_providers sp inner join user_personal_details  d "
+            . " FROM service_providers sp inner join user_personal_details d "
             . " using(user_id) where 1=1 " . $filter ;
 
         //die($rawQuery);
