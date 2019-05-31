0c7e0a5a6331e0d17bf3a9c7eaf39f1f0d425297
Antony Njoroge <antonjoro2008@gmail.com>
2019-05-31 23:39:41 +0300

    shortened message-3

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -35,7 +35,7 @@ class SMSController extends Controller
         $outbox->save();
 
         $sms = new SMS();
-        $smsSent = $sms->sendSMSMessage($recipients, $message, $outbox->id);
+        $smsSent = $sms->sendSMSMessage($recipients, $message, "UT".$outbox->id);
 
         if($smsSent){
 
