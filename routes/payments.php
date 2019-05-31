b9c9bcb6c5a1652ab388cc3e295add810b81c03b
Peter Murwa <wanjala.pet@gmail.com>
2019-05-31 22:24:08 +0300

    Fix mpesa route

--- routes/payments.php
+++ routes/payments.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::post('/mpesa/c2b/payment', 'PaymentsController@receive_mpesa');
-Route::post('/mpesa/c2b/process', 'PaymentsController@mpesa_payment');
+Route::post('/mpesa/c2b/process', 'PaymentsController@MpesaPayment');
 Route::post('/booking/checkstatus', 'PaymentsController@booking_status');
 Route::post('/mpesa/c2b/tips', 'PaymentsController@receive_mpesa_tips');
 Route::post('/api/sms/sendsms', 'SMSController@send_sms');
