cca7b921b25a948bb88a6c1a747a72ca38e0d186
Peter Murwa <wanjala.pet@gmail.com>
2019-06-14 22:56:16 +0300

    Debugging

--- resources/views/emails/booking/created.blade.php
+++ resources/views/emails/booking/created.blade.php
@@ -4,20 +4,19 @@
 Your booking has been received.
 
 @component('mail::table')
-|                  |                              |
-|:---------------- |:---------------------------- |
+|               |                 |
+|:------------- |:--------------- |
 | ** Service Provider ** | {{ $data['business_name'] }} |
 | ** Service  ** | {{ $data['service_name'] }} |
 | ** Service Cost  ** | {{ $data['service_cost'] }} |
 | ** Service Duration  ** | {{ $data['service_duration'] }} |
 | ** Service Duration  ** | {{ $data['service_duration'] }} (minutes) |
+| ** Booking Date  ** | {{ $data['booking_time'] }} (minutes) |
 @endcomponent
 
-#### Service Description
+### Service Description
 
 {{ $data['description'] }}
-
-** <u>Booking Date: {{ $data['booking_time'] }}</u> **
 
 To change or cancel your booking, log into [{{ config('app.name') }}]({{ config('app.url') }})  and select "My Bookings" bookmark
 
