f89fcb6d4907ec84aa094fc495b9f651b7e468fb
Peter Murwa <wanjala.pet@gmail.com>
2019-06-15 02:45:50 +0300

    Remove quotes on email

--- resources/views/emails/booking/accepted.blade.php
+++ resources/views/emails/booking/accepted.blade.php
@@ -6,19 +6,19 @@ Your booking for {{ $data['service_name'] }} from {{ $data['business_name'] }} h
 <h3 style="padding: 8px">Payment Information</h3>
 <ol>
 <li>
-    Go to Safaricom SIM Tool Kit, select <strong>M-PESA</strong> menu, select “<strong>Lipa</strong><strong> na </strong><strong>M-PESA</strong>“
+    Go to Safaricom SIM Tool Kit, select <strong>M-PESA</strong> menu, select <strong>Lipa</strong><strong> na </strong><strong>M-PESA</strong>
 </li>
-<li>Select “<strong>Pay Bill</strong>“</li>
-<li>Select “<strong>Enter Business no.</strong>“, Enter {{ config('app.name') }}’s Lipa na M-PESA PayBill Number
-    <span class="emphasize">{{ env('URBANTAP_PAYBILL') }}</span>&nbsp;and press “OK”
+<li>Select <strong>Pay Bill</strong></li>
+<li>Select <strong>Enter Business no.</strong>, Enter {{ config('app.name') }}’s Lipa na M-PESA PayBill Number
+    <span class="emphasize">{{ env('URBANTAP_PAYBILL') }}</span>&nbsp;and press OK
 </li>
-<li>Select “<strong>Enter Account no.</strong>“, Enter your <span
+<li>Select <strong>Enter Account no.</strong>, Enter your <span
             class="emphasize"> Booking Number </span> <span class="emphasize">{{ $data['booking_id'] }}</span> and
-    press “OK”
+    press OK
 </li>
-<li>“<strong>Enter Amount</strong>“, <span class="emphasize">{{ $data["service_cost"] }}</span> and press “OK”</li>
-<li><strong>Enter your M-PESA PIN</strong> and press “OK”</li>
-<li><strong>Confirm</strong> all the details are correct and press “OK”</li>
+<li><strong>Enter Amount</strong>, <span class="emphasize">{{ $data["service_cost"] }}</span> and press OK</li>
+<li><strong>Enter your M-PESA PIN</strong> and press OK</li>
+<li><strong>Confirm</strong> all the details are correct and press OK</li>
 <li>You will receive a <strong>confirmation SMS</strong> from M-PESA.</li>
 </ol>
 
