776735d02aa8083853b7eeb53acd37efd8b9cfe7
Peter Murwa <wanjala.pet@gmail.com>
2019-06-14 22:38:39 +0300

    Debugging

--- resources/views/emails/booking/created.blade.php
+++ resources/views/emails/booking/created.blade.php
@@ -4,9 +4,13 @@
 Your booking has been received.
 
 Service Provider:    **{{ $data['business_name'] }}**
+
 Service:            __ {{ $data['service_name'] }} __
+
 Service Cost:       ** {{ $data['service_cost'] }} **
+
 Service Duration:   ** {{ $data['service_duration'] }} (minutes)**
+
 Service Description:    {{ $data['description'] }}
 
 ** <u>Booking Date: {{ $data['booking_time'] }}</u> **
