0f9feaad2082df3f1b43b6e89c26583c4275fc36
Peter Murwa <wanjala.pet@gmail.com>
2019-06-15 02:56:54 +0300

    Debugging

--- resources/views/emails/booking/accepted.blade.php
+++ resources/views/emails/booking/accepted.blade.php
@@ -5,11 +5,11 @@ Your booking for {{ $data['service_name'] }} from {{ $data['business_name'] }} h
 
 ## Payment Information
 
-- Go to Safaricom SIM Tool Kit, select ** M-PESA ** menu, select ** Lipa ****  na  **** M-PESA **
+- Go to Safaricom SIM Tool Kit, select ** M-PESA ** menu, select ** Lipa na M-PESA **
 - Select ** Pay Bill **
 - Select ** Enter Business no. **, Enter {{ config('app.name') }} Lipa na M-PESA PayBill Number *{{ env('URBANTAP_PAYBILL') }}* and press OK
 - Select ** Enter Account no. **, Enter your * Booking Number * * {{ $data['booking_id'] }} * and press OK
-- ** Enter Amount **, *{{ $data["service_cost"] }} * and press OK
+- ** Enter Amount **, * {{ $data["service_cost"] }} * and press OK
 - ** Enter your M-PESA PIN ** and press OK
 - ** Confirm ** all the details are correct and press OK
 - You will receive a ** confirmation SMS ** from M-PESA.
