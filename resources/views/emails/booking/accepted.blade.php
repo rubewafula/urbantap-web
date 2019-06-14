954e5d5aedd682ab69e226833418cf16f00e6059
Peter Murwa <wanjala.pet@gmail.com>
2019-06-15 02:53:54 +0300

    Remove html - fallback to markdown

--- resources/views/emails/booking/accepted.blade.php
+++ resources/views/emails/booking/accepted.blade.php
@@ -3,24 +3,16 @@
 
 Your booking for {{ $data['service_name'] }} from {{ $data['business_name'] }} has been accepted. Please follow the instructions below to pay and confirm.
 
-<h3 style="padding: 8px">Payment Information</h3>
-<ol>
-<li>
-    Go to Safaricom SIM Tool Kit, select <strong>M-PESA</strong> menu, select <strong>Lipa</strong><strong> na </strong><strong>M-PESA</strong>
-</li>
-<li>Select <strong>Pay Bill</strong></li>
-<li>Select <strong>Enter Business no.</strong>, Enter {{ config('app.name') }} Lipa na M-PESA PayBill Number
-    <span class="emphasize">{{ env('URBANTAP_PAYBILL') }}</span>&nbsp;and press OK
-</li>
-<li>Select <strong>Enter Account no.</strong>, Enter your <span
-            class="emphasize"> Booking Number </span> <span class="emphasize">{{ $data['booking_id'] }}</span> and
-    press OK
-</li>
-<li><strong>Enter Amount</strong>, <span class="emphasize">{{ $data["service_cost"] }}</span> and press OK</li>
-<li><strong>Enter your M-PESA PIN</strong> and press OK</li>
-<li><strong>Confirm</strong> all the details are correct and press OK</li>
-<li>You will receive a <strong>confirmation SMS</strong> from M-PESA.</li>
-</ol>
+## Payment Information
+
+- Go to Safaricom SIM Tool Kit, select ** M-PESA ** menu, select ** Lipa ****  na  **** M-PESA **
+- Select ** Pay Bill **
+- Select ** Enter Business no. **, Enter {{ config('app.name') }} Lipa na M-PESA PayBill Number *{{ env('URBANTAP_PAYBILL') }}* and press OK
+- Select ** Enter Account no. **, Enter your * Booking Number * * {{ $data['booking_id'] }} * and press OK
+- ** Enter Amount **, *{{ $data["service_cost"] }} * and press OK
+- ** Enter your M-PESA PIN ** and press OK
+- ** Confirm ** all the details are correct and press OK
+- You will receive a ** confirmation SMS ** from M-PESA.
 
 You can also log into [{{ config('app.name') }}]({{ config('app.url') }}), select "My Bookings" bookmark and complete your payment
 
